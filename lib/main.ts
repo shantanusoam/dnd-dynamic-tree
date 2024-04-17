4a0d552e5390af388391ef8bd300b8a7ac903d7b
Shantanusoam <shantanusoam@mobikasa.com>
2024-04-17 08:31:04 +0530

    added styles and init codebase

---
+++ lib/main.ts
@@ -0,0 +1,18 @@
+
+import { SimpleTreeItemWrapper } from './dynamic-Tree/ui/simple/SimpleTreeItemWrapper';
+import { FolderTreeItemWrapper } from './dynamic-Tree/ui/folder/FolderTreeItemWrapper';
+import { SortableTree, SortableTreeProps } from './dynamic-Tree/SortableTree';
+import { flattenTree } from './dynamic-Tree/utilities';
+import type { TreeItems, TreeItemComponentProps, TreeItem } from './dynamic-Tree/types';
+
+export {
+  flattenTree,
+  SortableTree,
+  SimpleTreeItemWrapper,
+  FolderTreeItemWrapper,
+};
+export type { TreeItemComponentProps, TreeItems, TreeItem, SortableTreeProps };
+
+export function helloAnything(thing: string): string {
+    return `Hello ${thing}!`
+  }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
-
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] }),  libInjectCss()],
     build: {
+        copyPublicDir: false,
+        rollupOptions: {
+            external: ['react', 'react/jsx-runtime'],
+        },
        lib: {
           entry: resolve(__dirname, 'lib/main.ts'),
           formats: ['es']
