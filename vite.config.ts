c39f2e772d935e4fd0dbedc376ee52fa9daf56bd
Shantanusoam <shantanusoam@mobikasa.com>
2024-04-17 08:45:02 +0530

    added rollup config and css bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,22 @@ export default defineConfig({
         copyPublicDir: false,
         rollupOptions: {
             external: ['react', 'react/jsx-runtime'],
+            input: Object.fromEntries(
+                    glob.sync('lib/**/*.{ts,tsx}').map(file => [
+                      // The name of the entry point
+                      // lib/nested/foo.ts becomes nested/foo
+                      relative(
+                         'lib',
+                         file.slice(0, file.length - extname(file).length)
+                       ),
+                       // The absolute path to the entry file
+                      // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                      fileURLToPath(new URL(file, import.meta.url))
+                     ])),
+                         output: {
+                             assetFileNames: 'assets/[name][extname]',
+                            entryFileNames: '[name].js',
+                           }
         },
        lib: {
           entry: resolve(__dirname, 'lib/main.ts'),
